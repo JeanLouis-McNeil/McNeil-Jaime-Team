874bd0f85af387b25117b6f312a752fd67a4a680
JeanLouis-McNeil <jea15002@byui.edu>
2017-01-26 19:32:38 -0500

    01/26/2017 - A commit of the project The Last Of Us

---
+++ theLastOfUs/src/thelastofus/TheLastOfUs.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thelastofus;
+
+import byui.cit260.theLastOfUs.model.Player;
+
+/**
+ *
+ * @author iu
+ */
+public class TheLastOfUs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Player playerOne = new Player();
+        
+    playerOne.setName("McNeil Jean-Louis");
+    playerOne.setBestTime(8.00);
+    playerOne.setAge(24);
+    
+    String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
+        
+        Player playerTwo = new Player();
+        
+    playerTwo.setName("Jaime Reyes");
+    playerTwo.setBestTime(10.00);
+    playerTwo.setAge(30);
+    
+    String playerInfo2 = playerTwo.toString();
+        System.out.println(playerInfo2);
+    }
+    
+    
+    
+    
+}
