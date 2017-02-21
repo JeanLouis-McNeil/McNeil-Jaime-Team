856546b0dd8f1fad90b75b3f539c66f9ed46a2f8
JeanLouis-McNeil <jea15002@byui.edu>
2017-02-21 11:27:50 -0500

    01/26/2017 - A commit of the project The Last Of Us added the map class, the Location and the Character1 classes. I also added the Game and Scene class and the control layers and also the View Layer

--- theLastOfUs/src/byui/cit260/theLastOfUs/View/MainMenuView.java
+++ theLastOfUs/src/byui/cit260/theLastOfUs/View/MainMenuView.java
@@ -5,13 +5,96 @@
  */
 package byui.cit260.theLastOfUs.View;
 
+import java.util.Scanner;
+
 /**
  *
  * @author iu
  */
 public class MainMenuView {
+    private String menu;
+    private String promptMessage;
+    
+    public MainMenuView(){
+    this.menu = "\n"
+            + "\n--------------------------------------------------------------"
+            + "\n| Main Menu                                                  |"
+            + "\n--------------------------------------------------------------"
+            + "\nN - Start New Game                                            "
+            + "\nG - Get and Start saved game                                  "
+            + "\nH - Get help on how to play the game                          "
+            + "\nS - Save game                                                 "
+            + "\nQ - Quit                                                      "
+            ;
+    }
+    public void displayMainMenuView() {
+        
+        boolean done = false;
+        do {
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q"))
+                return;
+            done = this.doAction(menuOption);
+            }while(!done);
+    }
 
-    void displayMainMenuView() {
-    System.out.println("\n*** displayMainMenuView() function called ***");}
+    private String getMenuOption() {
+        Scanner keyboard =  new Scanner(System.in);//get infile for keyboard
+    String value = ""; //value to be returned
+    boolean valid = false; //initialize to not valid
+    
+    while (!valid) { 
+            System.out.println("\n" + this.promptMessage);
+    
+    value = keyboard.nextLine();
+    value = value.trim();
+    
+    if (value.length() > 1) {
+        System.out.println("\n Invalind value : Value cannot be more than one character.");
+        continue;
+        }
+        break;  
+    }
+    return value;
+    }
+
+    private boolean doAction(String menuOption) {
+          
+        menuOption = menuOption.toUpperCase();
+    switch (menuOption){
+        case "N":
+        this.startNewGame();
+        break;
+        case "G":
+        this.startExistingGame();
+        break;
+        case "H":
+        this.displayHelpMenu();
+        break;
+        case "S":
+        this.saveGame();
+        break;
+        default:
+            System.out.println("\n ***Invalid selection *** Try Again!");
+            break;
+            }  
+    return false;
+    }
+
+    private void startNewGame() {
+    System.out.println("\n*** startNewGame function called ***");
+    }
+
+    private void startExistingGame() {
+    System.out.println("\n*** startExistingGame function called ***");
+    }
+    
+    private void displayHelpMenu() {
+    System.out.println("\n*** displayHelpMenu function called ***");
+    }
+
+    private void saveGame() {
+    System.out.println("\n*** saveGame function called ***");
+    }
     
 }
