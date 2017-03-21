c36c60e70c93855ef7247b1f19dc2edf4d016c61
JASMINE <JASMINE@ANDRE>
2017-03-20 19:20:00 -0700

    I implemented the moveActorToStartingLocation() method and I began using the MapControlException class in the Exceptions package.

--- theLastOfUs/src/byui/cit260/theLastOfUs/control/MapControl.java
+++ theLastOfUs/src/byui/cit260/theLastOfUs/control/MapControl.java
@@ -5,8 +5,11 @@
  */
 package byui.cit260.theLastOfUs.control;
 
+import byui.cit260.theLastOfUs.Exceptions.MapControlException;
+import byui.cit260.theLastOfUs.model.Character1;
 import byui.cit260.theLastOfUs.model.Map;
 import byui.cit260.theLastOfUs.model.Scene;
+import java.awt.Point;
 import thelastofus.TheLastOfUs;
 
 /**
@@ -22,10 +25,22 @@ public class MapControl {
     return map;
     }
 
-    static void moveActorToStartingLocation(Map map) {
-        System.out.println("\nFunction successfully created");
-    
+    public static void moveActorToStartingLocation(Character1 character, Point coordinates) throws MapControlException {
+        Map map = TheLastOfUs.getCurrentGame().getMap();
+        int newRow = coordinates.x-1;
+        int newColumn = coordinates.y-1;
+        
+        if (newRow < 0 || newRow >= map.getRowCount() ||
+                newColumn < 0 || newColumn >=map.getColumnCount()) {
+        throw new MapControlException("Can not move charater to  location"
+                + coordinates.x + ", " + coordinates.y 
+                + "because that location is outside"
+                + "the bounds of the map");
+        }
+  
     }
+    
+    
     public enum SceneType{
     start,
     resources,
